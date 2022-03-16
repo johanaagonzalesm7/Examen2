d5abfc1bb1a7647289830de871f8b265ae758bbe
reyna <calzadareyna5@gmail.com>
2022-03-16 08:22:06 -0600

    Se agrego area

--- Exam2.py
+++ Exam2.py
@@ -1,3 +1,8 @@
 #Numeros Fibonacci
 
 #Area de un Circulo
+import areadecirculo
+#llamando a las funciones contenidas 
+#en modulo
+print("Trabajando la funcion en mi modulo")
+areadecirculo.areacirculo()
